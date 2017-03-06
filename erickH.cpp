281b51b3fa27dce96835fe3ec5f701430e3f6f62
Erick Herrera <erick_perez_herrera@outlook.com>
2017-03-06 06:46:53 -0800

    Update erickH.cpp

--- erickH.cpp
+++ erickH.cpp
@@ -7,6 +7,9 @@
 #include <GL/glx.h>
 using namespace std;
 
+void GAMEOVERSCREEN (void);
+void showGAMEOVERSCREEN ();
+
 void Maverick () {
     for (int i = 0; i < 10; i++) {
 	    cout << "mavERICK";
@@ -25,4 +28,34 @@ void Maverick2 (int yres) {
     maverick.left = 850;
     maverick.center = 0;
     ggprint16(&maverick, 0, 0x00ffffff, name);
+}
+
+void MaverickHealth (float health) {
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex2f(0, 0);
+	glVertex2f(1, 0);
+	glVertex2f(2, 0);
+	glVertex2f(3, 0);
+	glVertex2f(4, 0);
+	glVertex2f(5, 0);
+	glVertex2f(1, health);
+	glVertex2f(0, health);
+	glEnd();
+}
+
+struct BACKGROUNDIMAGE {
+	int x;
+	int y;
+	char *image;
+} BACKGROUNDIMAGE;
+    
+void GAMEOVERSCREEN (void) {
+	glEnable(GL_TEXTURE_2D);
+	loadTexture("images/gameover.png");
+}
+
+void showGAMEOVERSCREEN () {
+
+
 }
